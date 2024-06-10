ab4bfcf81dc6d4bfe820010221fcf466eedf3500
adamandp <adamanandaputra@gmail.com>
2024-06-10 13:00:22 +0700

    update

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <p class="bg-blue-300">hello world</p>
+  <div class="bg-green-500">hellow</div>
+</template>
+
+<style scoped>
+
+</style>
